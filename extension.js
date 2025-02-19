f80633d0e806ef91b9341f5fe2a343930b988bc8
fthx <23138504+fthx@users.noreply.github.com>
2025-02-19 11:23:51 +0100

    Remove buggy spinner from indicator

--- extension.js
+++ extension.js
@@ -9,37 +9,41 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
 export default class AppMenuIsBackExtension {
-    _movePlacesMenu() {
-        let placesIndicator = Main.panel.statusArea['places-menu'];
-
-        if (placesIndicator) {
-            Main.panel._leftBox.remove_child(placesIndicator.container);
-            Main.panel._leftBox.insert_child_at_index(placesIndicator.container, 1);
-        }
-    }
-
-    enable() {
-        if (!Main.sessionMode.panel.left.includes('appMenu')) {
-            Main.sessionMode.panel.left.push('appMenu');
-            Main.sessionMode._sync();
-        }
-
+    _shiftPlacesMenu() {
         this._timeout = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
-            this._movePlacesMenu();
+            let placesIndicator = Main.panel.statusArea['places-menu'];
+
+            if (placesIndicator) {
+                Main.panel._leftBox.remove_child(placesIndicator.container);
+                Main.panel._leftBox.insert_child_at_index(placesIndicator.container, 1);
+            }
 
             this._timeout = null;
             return GLib.SOURCE_REMOVE;
         });
     }
 
+    enable() {
+        if (!Main.sessionMode.panel.left.includes('appMenu')) {
+            Main.sessionMode.panel.left.push('appMenu');
+            Main.panel._updatePanel();
+
+            Main.panel.statusArea.appMenu._container.remove_child(Main.panel.statusArea.appMenu._spinner);
+        }
+
+        this._shiftPlacesMenu();
+    }
+
     disable() {
         if (Main.sessionMode.panel.left.includes('appMenu')) {
             Main.sessionMode.panel.left.pop();
-            Main.sessionMode._sync();
+            Main.panel._updatePanel();
+
+            Main.panel.statusArea.appMenu._container.add_child(Main.panel.statusArea.appMenu._spinner);
             Main.panel.statusArea.appMenu?.destroy();
         }
 
-        this._movePlacesMenu();
+        this._shiftPlacesMenu();
 
         if (this._timeout) {
             GLib.Source.remove(this._timeout);
